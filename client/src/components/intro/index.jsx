53acc67d4efe21d397e80d374900e89e26fe0d6b
Ömer Can Parlak <65744997+OmerCanParlak@users.noreply.github.com>
2023-11-06 15:03:28 +0300

    Update index.jsx

--- client/src/components/intro/index.jsx
+++ client/src/components/intro/index.jsx
@@ -1,53 +1,60 @@
-'use client';
-import React, { useLayoutEffect, useRef } from 'react'
 import styles from './style.module.css';
 import Image from 'next/image';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useLayoutEffect, useRef } from 'react';
 
-export default function Index() {
-
-    const background = useRef(null);
+export default function index() {
+    
+    const backgroundImage = useRef(null);
     const introImage = useRef(null);
+    const app = useRef();
 
-    useLayoutEffect( () => {
-        gsap.registerPlugin(ScrollTrigger);
+    useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+            gsap.registerPlugin(ScrollTrigger);
+            const timeLine = gsap.timeline({
+                scrollTrigger: {
+                    trigger: document.documentElement,
+                    start: "top",
+                    end: "+=500px",
+                    scrub: true,
+                    
+                }
+            });
+    
+            timeLine
+                .to(backgroundImage.current,{clipPath:"inset(0%)"},0)
+                .to(introImage.current,{height:"200px"},0)
+        },app)
+        
+        return () => ctx.revert()
 
-        const timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: document.documentElement,
-                scrub: true,
-                start: "top",
-                end: "+=500px",
-            },
-        })
-
-        timeline
-            .from(background.current, {clipPath: `inset(15%)`})
-            .to(introImage.current, {height: "200px"}, 0)
     }, [])
-
+  
     return (
-        <div className={styles.homeHeader}>
-            <div className={styles.backgroundImage} ref={background}>
-                <Image 
+        <div className={styles.intro} ref={app}>
+            <div  ref={backgroundImage}  className={styles.backgroundImage}>
+                <Image
                     src={'/images/background.jpeg'}
                     fill={true}
-                    alt="background image"
-                    priority={true}
+                    alt='background image'
                 />
             </div>
-            <div className={styles.intro}>
-                    <div ref={introImage} data-scroll data-scroll-speed="0.3" className={styles.introImage}>
-                        <Image
-                            src={'/images/intro.png'}
-                            alt="intro image"
-                            fill={true} 
-                            priority={true}
-                        />
-                    </div>
-                    <h1 data-scroll data-scroll-speed="0.7">SMOOTH SCROLL</h1>
-             </div>
+            <div  className={styles.introContainer}>
+                <div ref={introImage} data-scroll data-scroll-speed="0.3" className={styles.introImage}>
+                    <Image
+                        src={'/images/intro.png'}
+                        fill={true}
+                        alt="intro image"
+                    />
+                </div>
+                <h1 data-scroll data-scroll-speed="0.7">
+                    Smooth Scroll
+                </h1>
+            </div>
+
+
         </div>
     )
 }
